91891f3773451f500d166dbd49c11f1df9c76d87
Asen Todorov <astodorov@outlook.com>
2016-03-13 11:02:28 +0200

    Add material for ball,

--- Assets/Scripts/Ball/Balls.cs
+++ Assets/Scripts/Ball/Balls.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Balls : MonoBehaviour {
+public class Balls : MonoBehaviour
+{
 
-    public static int BALL_FUll_FILL = 0;
-    public static int BALL_HALF_FILL = 1;
+	public static int BALL_FUll_FILL = 0;
+	public static int BALL_HALF_FILL = 1;
+	public Rigidbody rigidbody;
 
+	void Awake()
+	{
+		Invoke("HitBall", 1f);  // Calls HitBall in 1 second.
+	}
+	private void HitBall()
+	{
+		rigidbody.AddForce(new Vector3(-5f, 0f, 3f), ForceMode.Impulse);
+	}
 }
