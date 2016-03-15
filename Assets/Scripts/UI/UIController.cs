56aabef63a76c49092cef94ee7574e3ab8b9d2b3
akozev <akozev@gmail.com>
2016-03-15 11:07:01 +0200

    ADD Player's turn text: must call UIController ref StartCoroutine(PlayersTurn(2)); ADD Game Finished text: must call UIController ref StartCoroutine(GameFinished(playerName));

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -8,14 +8,23 @@ public class UIController : MonoBehaviour {
     public GameObject screenOptions;
     public GameObject UIScreens;
     public GameObject UIInGame;
-   
+    public Text playersTurn;
 
     private GameObject currentScreen;
+    private Animator playerTurnAnimation;
+    
 
 	// Use this for initialization
 	void Start () {
         currentScreen = screenWellcome;
+        playerTurnAnimation = playersTurn.gameObject.GetComponent<Animator>();
 
+        playersTurn.gameObject.SetActive(false);
+
+        //PlayersTurn(2);
+        //StartCoroutine(PlayersTurn(2));
+
+        //StartCoroutine(GameFinished("David"));
     }
 
     public void ShowOptionsScreen ()
@@ -43,6 +52,24 @@ public class UIController : MonoBehaviour {
         Application.Quit();
     }
 
-   
+    public IEnumerator PlayersTurn (int playerNum)
+    {
+        playersTurn.text = "Player's " + playerNum + " Turn";
+        playersTurn.gameObject.SetActive(true);
+        playerTurnAnimation.SetBool("IsHide", false);
+
+        yield return new WaitForSeconds(3);
+        playerTurnAnimation.SetBool("IsHide", true);
+    }
+
+    public IEnumerator GameFinished (string playerName)
+    {
+        playersTurn.text = playerName +" WON";
+        playersTurn.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+        playerTurnAnimation.SetBool("IsHide", true);
+
+    }
 
 }
