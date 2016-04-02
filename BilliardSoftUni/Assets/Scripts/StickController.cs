b8b8b2958b194fe21315f3d41525fff17600c0a8
DannyHr <dani_a1@ymail.com>
2016-04-02 19:15:50 +0300

    Implemented white ball hit according to direction of the stick

--- BilliardSoftUni/Assets/Scripts/StickController.cs
+++ BilliardSoftUni/Assets/Scripts/StickController.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 public class StickController : MonoBehaviour
 {
+    public const float Force = 90.0f;
+
     public Transform WhiteBallTransform;
     public Rigidbody WhiteBallRigidbody;
     public float RotationSpeed;
@@ -28,7 +31,7 @@ public class StickController : MonoBehaviour
         var allBalls = new List<GameObject>();
         for (int i = 0; i < Balls.transform.childCount; i++)
         {
-            if (Balls.transform.GetChild(i).GetComponent<MeshRenderer>().enabled)
+            if (Balls.transform.GetChild(i).gameObject.activeSelf)
             {
                 allBalls.Add(Balls.transform.GetChild(i).gameObject);
             }
@@ -56,8 +59,17 @@ public class StickController : MonoBehaviour
 
             Debug.Log("Pressed S");
 
-            var stickForce = new Vector3(90f, 0f, -0.3f);
-            StartCoroutine(HitBall(stickForce, 1f));
+            var stickPosition = this.stick.transform.position;
+            var ballPosition = this.WhiteBallTransform.position;
+
+            var force = ballPosition - stickPosition;
+
+            var multiplier = 1.0f / (Mathf.Abs(force.x) + Mathf.Abs(force.z));
+            var forceX = force.x * multiplier;
+            var forceZ = force.z * multiplier;
+
+            var forceVector = new Vector3(forceX * Force, -0.3f, forceZ * Force);
+            StartCoroutine(HitBall(forceVector, 1f));
         }
 
         if (Input.GetKeyUp(KeyCode.S))
@@ -85,7 +97,6 @@ public class StickController : MonoBehaviour
     {
         yield return new WaitForSeconds(delayTime);
 
-        //ballBody.AddForce(new Vector3(100f, 0f, -0.3f), ForceMode.Impulse);
         this.WhiteBallRigidbody.AddForce(force, ForceMode.Impulse);
     }
 }
